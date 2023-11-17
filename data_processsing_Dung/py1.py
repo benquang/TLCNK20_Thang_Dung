04c28cbd9bc1901b920ccffb24874a17d0eadd0e
benquang <114910044+benquang@users.noreply.github.com>
2023-11-17 21:28:25 +0700

    Delete data_processsing_Dung directory

--- data_processsing_Dung/py1.py
+++
@@ -1,9 +0,0 @@
-#Quick sort function
-def quicksort(arr):
-    if len(arr) < 2:
-        return arr
-    else:
-        pivot = arr[0]
-        less = [i for i in arr[1:] if i <= pivot]
-        greater = [i for i in arr[1:] if i > pivot]
-        return quicksort(less) + [pivot] + quicksort(greater)
